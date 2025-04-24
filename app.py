83915f4f2e832d40585f4b3479926d3bb8e512df
juanreina <juandavidreina.ms@gmail.com>
2025-04-23 23:59:50 -0500

    feat(task): create task function

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from func import create-task
+from func import create_task
 
-
-def menu():
+def main():
     while True:
-        op = int(input("1. crear"))
-        if op == 1:
-            i = str(input("escriba descripcion"))
-            create_task(i)
+        print(f'\n**TASK MANAGER**\n1. Create task\n2. Remove task\n3. Show tasks\n4. Mark task as completed\n5. Exit')
+        op = int(input("Option: "))
+        match op:
+            case 1:
+                task = str(input("Write the description of your task: "))
+                create_task.create_task(task)
+            case 2:
+                break
+            case 3:
+                break
+            case 4:
+                break
+            case 5:
+                print(f'\nYou have exited correctly.')
+                return False
+            
+main()
+
--- func/create_task.py
+++ func/create_task.py
@@ -1,13 +1,14 @@
-from save import tasks
+from save import tasks as taskdb
 import uuid
 
 def generated_unique_id():
     return str(uuid.uuid4())
 
-def create_task(description):
+def create_task(description: str):
     new_task = {
         "id": generated_unique_id(),
         "description": description,
         "completed": False
     }
-    tasks.append(new_task)
+    taskdb.tasks.append(new_task)
+    print(f'Your task was added successfully')
